4df42ac2676f8ac05b2aa0369b13b82bf8ceb0df
hayfahamoudi <91759804+hayfahamoudi@users.noreply.github.com>
2021-10-31 00:10:43 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import Pictogramme from './composant/Pictogramme.js';
+import Commentaire from './composant/Commentaire.js';
+import Icone from './composant/Iconne.js';
+import './App.css';
+handleClick() 
+{
+
+  <p>Premier defit reussi"</p>
+}
+render()
+ {
+  return (
+    <div class="col-6 offset-3">
+      <div class="card">
+        <div class="card-block">
+          <div class="row">
+            <div class="col-2">
+              <img src="https://thomas-bart.com/wp-content/uploads/2017/03/developper-1680x1201.jpg"  class="rounded float-left" class="img-thumbnail" width="80" height="70" />
+            </div>
+            <div class="col-10 profile-row">
+              <div class="row">
+                <a href="#"><h1> Débuter avec React </h1></a>
+              </div>
+            </div>
+          </div>
+          <p>Bonne découverte des composants React "Components!!!"</p>
+      
+          <div>
+            <Pictogramme />
+          </div>
+        </div>
+  
+        <div class="card-footer text-muted">
+         <Commentaire />
+        </div>
+  
+      </div>
+    </div>
+      )
+  }
+  
+
+export default App;
